d6d46352e92dca86880dfb8af7e940fc7a4009c4
242165-arch <239209852+242165-arch@users.noreply.github.com>
2026-01-08 14:20:37 -0600

    make passwords cleaner by automaticaqlly banning certain chars

--- features/PassGen/passgen.py
+++ features/PassGen/passgen.py
@@ -7,7 +7,9 @@ def back_to_main():
   run.run_path("./main.py")
 def get_user_input():
     length = int(input("Enter desired password length: "))
-    chars_to_avoid = input("Enter characters to avoid (leave blank if none): ")
+    chars_to_avoid1 = input("Enter characters to avoid (leave blank if none): ")
+    chars_to_avoid2 = "/.<>,`~][}{]|"
+    chars_to_avoid = chars_to_avoid1 + chars_to_avoid2
     word_to_include = input("Enter a word to include in the password (leave blank if none): ")
 
     if len(word_to_include) > length:
